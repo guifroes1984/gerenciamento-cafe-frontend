6e240cc4df175ea37d4b710eb825d3262161a14e
guifroes1984 <guilhermefroes26@gmail.com>
2023-08-12 00:52:31 -0300

    Implementado o alterar senha e o logout

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -35,4 +35,11 @@ export class UserService {
   checkToken() {
     return this.httpClient.get(this.url + "/user/checkToken");
   }
+
+  changePassord(data: any) {
+    return this.httpClient.post(this.url +
+      "/user/changePassword/", data, {
+        headers: new HttpHeaders().set('Content-Type', 'application/json')
+      })
+  }
 }
